7e03ab22fce1eec1b6f769f4e2d987c98b48f031
michapau <raspar_123@yahoo.fr>
2024-03-08 19:43:50 +0100

    builder update

--- src/lib.rs
+++ src/lib.rs
@@ -12,26 +12,34 @@ use xpdf_info::PdfInfo;
 
 use types::XpdfArgs;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum PdfErrorKind {
     NoXpdfToolsDirectory,
+    ToolNotAvailable,
     PdfInfoError,
     PdfToTextError,
     PdfToTextErrorNoOutput,
 }
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct PdfError {
     pub message: String,
     pub process_message: String,
     pub error_kind: PdfErrorKind,
 }
 
+impl PdfError {
+    fn kind(&self) -> PdfErrorKind {
+        self.error_kind
+    }
+}
 impl fmt::Display for PdfError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.message)
     }
 }
 impl std::error::Error for PdfError {}
+
+const VALID_TOOLS: &'static [&'static str] = &["pdfdetach", "pdffonts", "pdfimages", "pdfinfo", "pdftohtml", "pdftopng", "pdftoppm", "pdftops", "pdftotext"];
 
 #[derive(Debug)]
 pub struct XpdfTools {
@@ -47,26 +55,48 @@ impl XpdfTools {
     }
 
     pub fn pdf_info(&self, pdf_file: &Path) -> Result<PdfInfo, PdfError> {
-        xpdf_info::pdf_info(pdf_file, &self)
+
+        match Self::pdf_check_tool(self, "pdfinfo") {
+            Ok(_) => xpdf_info::pdf_info(pdf_file, &self),
+            Err(e) => Err(e),
+        }
+    }
+    pub fn pdf_text(&self, pdf_file: &Path) -> Result<Vec<u8>, PdfError> {
+       
+        match Self::pdf_check_tool(self, "pdfinfo") {
+            Ok(_) =>  xpdf_text::pdf_to_binary(pdf_file, &self),
+            Err(e) => Err(e),
+        }
     }
 
-    pub fn pdf_text(&self, pdf_file: &Path) -> Result<Vec<u8>, PdfError> {
-        xpdf_text::pdf_to_binary(pdf_file, &self)
+    pub fn pdf_check_tool(&self, tool: &str) -> Result<bool, PdfError> {
+        if self.available_tools.as_ref().unwrap().contains(&tool.to_string()) {
+            Ok(true)
+        } else {
+            Err(Self::tool_not_available(tool))
+        }
+    }
+    fn tool_not_available(tool: &str) -> PdfError {
+        PdfError {
+            message: format!("{} not found in tools folder", tool),
+            process_message: "".into(),
+            error_kind: PdfErrorKind::ToolNotAvailable,
+        }
     }
 }
+#[derive(Debug)]
 pub struct XpdfToolsBuilder {
     tools_folder: PathBuf,
     extra_args: Option<Vec<XpdfArgs>>,
     available_tools: Option<Vec<String>>,
 }
 
-//const VALID_TOOLS: [&'static str; 9] = ["pdfdetach", "pdffonts", "pdfimages", "pdfinfo", "pdftohtml", "pdftopng", "pdftoppm", "pdftops", "pdftotext"];
-const VALID_TOOLS: &'static [&'static str] = &["pdfdetach", "pdffonts", "pdfimages", "pdfinfo", "pdftohtml", "pdftopng", "pdftoppm", "pdftops", "pdftotext"];
 impl XpdfToolsBuilder {
+
     fn new(tools_folder: PathBuf) -> Result<Self, PdfError> {
 
         if tools_folder.is_dir() {
-            //let valid_tools = vec!["pdfdetach", "pdffonts", "pdfimages", "pdfinfo", "pdftohtml", "pdftopng", "pdftoppm", "pdftops", "pdftotext"];
+           
             let dir = tools_folder.read_dir().unwrap();
 
             //prop check https://github.com/rust-lang/libs-team/issues/311 for a better way to do this..
@@ -74,7 +104,6 @@ impl XpdfToolsBuilder {
                 .filter_map(|entry| entry.ok())
                 .map(|entry| entry.path().file_stem().unwrap().to_owned())
                 .filter_map(|stem| stem.into_string().ok())
-                //.filter(|s| valid_tools.contains(&s.as_str()))
                 .filter(|s| VALID_TOOLS.contains(&s.as_str()))
                 .collect();
             
@@ -89,18 +118,12 @@ impl XpdfToolsBuilder {
                 process_message: "".into(), 
                 error_kind: PdfErrorKind::NoXpdfToolsDirectory })
         }
-        
     }
 
     pub fn extra_args(mut self, extra_args: Vec<XpdfArgs>) -> Self {
         self.extra_args = Some(extra_args);
         self
     }
-
-    // pub fn tools_folder(mut self, tools_folder: &str) -> Self {
-    //     self.tools_folder = PathBuf::from(tools_folder);
-    //     self
-    // }
 
     pub fn build(self) -> XpdfTools {
         XpdfTools { extra_args: self.extra_args, tools_folder: self.tools_folder, available_tools: self.available_tools}
@@ -114,5 +137,18 @@ pub fn get_version() -> String {
 
 #[test]
 fn test_builder_errors() {
+    let tools_result = XpdfTools::builder(PathBuf::from("./folder/not/there"));
+    assert_eq!(tools_result.unwrap_err().error_kind, PdfErrorKind::NoXpdfToolsDirectory);
 
+    let tools_result = XpdfTools::builder(PathBuf::from("./testData/binTester"));
+    assert!(tools_result.unwrap().available_tools.unwrap().len() == 2);
+    
+}
+
+#[test]
+fn test_tool_available() {
+    let tools_result = XpdfTools::builder(PathBuf::from("./testData/binTester")).unwrap().build();
+    assert_eq!(tools_result.pdf_check_tool("pdfinfo"), Ok(true));
+    assert_eq!(tools_result.pdf_check_tool("pdftotext"), Ok(true));
+    assert_eq!(tools_result.pdf_check_tool("pdftoWindmill").unwrap_err().kind(), PdfErrorKind::ToolNotAvailable);
 }
